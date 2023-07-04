00c752a99ff4b7c087c7cb6f295913304fdbcbb0
Svetlana-18 <vasilevichsvetlana@yandex.ru>
2023-07-04 19:51:25 +0300

    homework5

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import Solution.HeapSort;
+import Solution.Task0;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // System.out.println("Hello, World!");
+        Task0.main(args);
+        HeapSort.main(args);
+    }
+}
---
+++ src/Solution/HeapSort.java
@@ -0,0 +1,71 @@
+// 2) Реализовать алгоритм пирамидальной сортировки (HeapSort).
+
+package Solution;
+
+public class HeapSort {
+    public void sort(int arr[]) {
+        int n = arr.length;
+
+        // Построение кучи (перегруппируем массив)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        // Один за другим извлекаем элементы из кучи
+        for (int i = n - 1; i >= 0; i--) {
+            // Перемещаем текущий корень в конец
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // Вызываем процедуру heapify на уменьшенной куче
+            heapify(arr, i, 0);
+        }
+    }
+
+    // Процедура для преобразования в двоичную кучу поддерева с корневым узлом i,
+    // что является
+    // индексом в arr[]. n - размер кучи
+    void heapify(int arr[], int n, int i) {
+        int largest = i; // Инициализируем наибольший элемент как корень
+        int l = 2 * i + 1; // левый = 2*i + 1
+        int r = 2 * i + 2; // правый = 2*i + 2
+
+        // Если левый дочерний элемент больше корня
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        // Если правый дочерний элемент больше, чем самый большой элемент на данный
+        // момент
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+        // Если самый большой элемент не корень
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Рекурсивно преобразуем в двоичную кучу затронутое поддерево
+            heapify(arr, n, largest);
+        }
+    }
+
+    /* Вспомогательная функция для вывода на экран массива размера n */
+    static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    // Управляющая программа
+    public static void main(String args[]) {
+        int arr[] = { 17, 1, 3, 9, 5, 14 };
+        System.out.println("Array ");
+        printArray(arr);
+
+        HeapSort ob = new HeapSort();
+        ob.sort(arr);
+
+        System.out.println("Sorted array is");
+        printArray(arr);
+    }
+}
